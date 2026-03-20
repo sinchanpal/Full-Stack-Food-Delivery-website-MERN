f34d82fd06e557c7f61ec2b23eba7da4ea38b603
Sinchan Pal <sinchanpal22@gmail.com>
2026-03-21 01:38:59 +0530

    change host in sendMail.js

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
